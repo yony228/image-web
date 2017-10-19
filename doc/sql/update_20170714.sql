59a1c8d8bef9402f0489a02bad19738ccf421b9e
zll405361679 <zll405361679@163.com>
2017-10-19 15:30:37 +0800

    init data

--- doc/sql/update_20170714.sql
+++
@@ -1,9 +0,0 @@
-#20170712 修改
-ALTER TABLE `classifications`
-MODIFY COLUMN `model_id`  int(11) NULL DEFAULT 0 AFTER `des`;
-
-update classifications set model_id=0 where model_id is null;
-
-#20170714 修改
-ALTER TABLE `users`
-ADD COLUMN `status`  int(2) NULL DEFAULT 0 COMMENT '状态（0:正常，1:禁用）' AFTER `password`;
--- doc/sql/update_20170727.sql
+++
@@ -1,4 +0,0 @@
-#20170727
-ALTER TABLE `models`
-ADD COLUMN `show_rate_top`  float(5,2) NULL DEFAULT 0.7 AFTER `status`,
-ADD COLUMN `show_rate_bottom`  float(5,2) NULL DEFAULT 0.3 AFTER `show_rate_top`;
